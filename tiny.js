ebf894682b8712d01ffd3901a5216cdbf688ce6b
adcident12 <santikon12@gmail.com>
2018-11-15 23:25:49 +0700

    commit 2

--- tiny.js
+++ tiny.js
@@ -1,23 +1,27 @@
+// @ts-ignore
 $(document).ready(function(){
     //ดึงข้อมูลจาก php
     var MentionsServer = function (term, success) {
+        // @ts-ignore
         $.getJSON('/data.php', function (data) {
-            var userNames = data;
-            var users = userNames.map(function (fullName) {
-                var name = fullName;
+            var users = data.map(function (result) {
+                var id = result.id;
+                var name = result.name.toLowerCase().replace(/ /g, '');
+                var fullname = result.name;
+                var images = result.images;
                 return {
-                    id: name,
-                    name,
-                    fullName,
+                    id: id,
+                    name: name,
+                    fullName: fullname, 
+                    image: images,
                 };
+                
             });
-
             var findUser = function (term, success) {
-                /* demo string search */
                 var matches = users.filter(function (user) {
-                    return user.name.indexOf(term.toLowerCase()) !== -1;
+                    return user.name.indexOf(term.toLowerCase()) === 0;
                 });
-                /* fake async server delay */
+                matches = matches.slice(0, 10);
                 var timeout = 30;
                 window.setTimeout(function () {
                     success(matches);
@@ -30,22 +34,24 @@ $(document).ready(function(){
     //autocomplete
     var mentions_menu_complete = function (editor, userinfo) {
         const x = document.createElement('div');
-        x.innerHTML = '<span style="color: green" class="mentionsmentionsmentions">@' + userinfo.name + '</span>';
+        x.innerHTML = '<span style="color: green" class="mentionsmentionsmentions"><img src="'+userinfo.image+'" style="width: 50px; height: 50px; border-radius: 25px 25px; position: relative; top: 15px;">'+" "+userinfo.fullName+'</span>';
         return x.childNodes[0];
     };
 
     var mentions_fetch = function (query, success) {
         MentionsServer(query.term, success);
+        console.log(success);
     };
 
 //TINYMCE
+    // @ts-ignore
     tinymce.init({
         selector: "textarea",
-        plugins: "mentions lists emoticons",
-        toolbar: "numlist bullist emoticons",
-        fullpage_default_doctype: "<!DOCTYPE html>",
+        themes: "modern",
+        plugins: "mentions lists emoticons code",
+        toolbar: "numlist bullist emoticons code",
         mentions_selector: '.mentionsmentionsmentions',
         mentions_fetch: mentions_fetch,
-        mentions_menu_complete: mentions_menu_complete
+        mentions_menu_complete: mentions_menu_complete,
     });
 });
